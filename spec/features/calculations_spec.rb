99740af68ae45145d2a6f864e5c003367277206d
Raghu Betina <raghu@firstdraft.com>
2017-04-22 11:24:40 -0500

    More helpful specs and hints

--- spec/features/calculations_spec.rb
+++ spec/features/calculations_spec.rb
@@ -1,226 +1,342 @@
 require "rails_helper"
 
 feature "Flexible square" do
-  it "works with input 5", points: 1 do
+  it "works with input 5", points: 2, hint: h("params_are_strings") do
     visit "/flexible/square/5"
 
-    expect(page).to have_content("25")
+    expect(page).to have_content(25)
   end
+end
 
-  it "works with any input", points: 5 do
-    random_input = rand(100)
+feature "Flexible square" do
+  it "works with input 42", points: 4, hint: h("params_are_strings") do
+    visit "/flexible/square/42"
 
-    visit "/flexible/square/#{random_input}"
-
-    expected_output = random_input ** 2
-
-    expect(page).to have_content(expected_output)
+    expect(page).to have_content(1764)
   end
 end
 
 feature "Flexible square root" do
-  it "works with input 8", points: 1 do
-    visit "/flexible/square_root/8"
+  it "works with input 5", points: 2, hint: h("params_are_strings") do
+    visit "/flexible/square_root/5"
 
-    expect(page).to have_content("2.83")
+    expect(page).to have_content(2.236)
   end
+end
 
-  it "works with any input", points: 5 do
-    random_input = rand(100)
+feature "Flexible square root" do
+  it "works with input 42", points: 2, hint: h("params_are_strings") do
+    visit "/flexible/square_root/42"
 
-    visit "/flexible/square_root/#{random_input}"
-
-    expected_output = (random_input ** 0.5).round(2)
-
-    expect(page).to have_content(expected_output)
+    expect(page).to have_content(6.4807)
   end
 end
 
 feature "Flexible payment" do
-  it "works", points: 5 do
+  it "works", points: 5, hint: h("params_are_strings") do
+    visit "/flexible/payment/324/5/14600"
+
+    expect(page).to have_content(263.90)
+  end
+end
+
+feature "Flexible payment" do
+  it "rounds the payment", points: 1, hint: h("round") do
     visit "/flexible/payment/324/5/14503"
 
-    expect(page).to have_content("262.15")
+    expect(page).to have_content(262.15)
   end
+end
 
-  it "formats large payments", points: 2, hint: I18n.t("hints.number_to_currency") do
+feature "Flexible payment" do
+  it "formats large dollar amounts", points: 2, hint: h("number_to_currency") do
     visit "/flexible/payment/410/30/250000"
 
     expect(page).to have_content("$1,208.00")
   end
 end
 
-feature "Square form" do
+feature "Flexible payment" do
+  it "formats the rate as a percentage", points: 1, hint: h("number_to_percentage") do
+    visit "/flexible/payment/410/30/250000"
+
+    expect(page).to have_content("4.10%")
+  end
+end
+
+feature "First RCAV for square form" do
   it "has a form element", points: 1 do
     visit "/square/new"
 
-    expect(page).to have_css("form")
+    expect(page).to have_css("form", count: 1)
   end
+end
 
-  it "has a label", points: 2 do
+feature "First RCAV for square form" do
+  it "has a label", points: 1, hint: h("copy_must_match") do
     visit "/square/new"
 
     expect(page).to have_css("label", text: "Enter a number")
   end
+end
 
-  it "has an input", points: 2 do
+feature "First RCAV for square form" do
+  it "has an input", points: 1 do
     visit "/square/new"
 
-    expect(page).to have_css("input")
+    expect(page).to have_css("input", count: 1)
   end
+end
 
-  it "has a button with the correct copy", points: 1, hint: I18n.t("hints.button_text") do
+feature "First RCAV for square form" do
+  it "has a button", points: 1, hint: h("copy_must_match") do
     visit "/square/new"
 
     expect(page).to have_css("button", text: "Calculate square")
   end
+end
 
-  it "when submitted leads to some other URL", points: 4 do
+feature "First RCAV for square form" do
+  it "has an action attribute", points: 1 do
     visit "/square/new"
 
     expect(page).to have_css("form[action]")
   end
+end
 
-  it "captures the user's input in the query string with a name", points: 4 do
+feature "First RCAV for square form" do
+  it "when submitted leads to another functional RCAV", points: 6, hint: h("button_type") do
     visit "/square/new"
 
-    expect(page).to have_css("input[name]")
-  end
+    click_button "Calculate square"
 
-  it "works with 5", points: 6, hint: I18n.t("hints.label_for_input") do
+    expect(page).to_not have_current_path("/square/new", only_path: true)
+  end
+end
+
+feature "First RCAV for square form" do
+  it "captures the user's input in the query string", points: 4, hint: h("names_for_inputs") do
+    visit "/square/new"
+
+    expect(page).to have_css("input[name]", count: 1)
+  end
+end
+
+feature "Second RCAV for square form" do
+  it "correctly processes an input of 5", points: 3, hint: h("label_for_input params_are_strings") do
     visit "/square/new"
 
     fill_in "Enter a number", with: 5
 
     click_button "Calculate square"
 
-    expect(page).to have_content("25.0")
-  end
-
-  it "works with 6", points: 6, hint: I18n.t("hints.label_for_input") do
-    visit "/square/new"
-
-    fill_in "Enter a number", with: 6
-
-    click_button "Calculate square"
-
-    expect(page).to have_content("36.0")
+    expect(page).to have_content(25)
   end
 end
 
-feature "Square root form" do
+feature "Second RCAV for square form" do
+  it "correctly processes an input of 42.42", points: 3, hint: h("label_for_input params_are_strings") do
+    visit "/square/new"
+
+    fill_in "Enter a number", with: 42.42
+
+    click_button "Calculate square"
+
+    expect(page).to have_content(1799.4564)
+  end
+end
+
+feature "First RCAV for square root form" do
   it "has a form element", points: 1 do
     visit "/square_root/new"
 
-    expect(page).to have_css("form")
+    expect(page).to have_css("form", count: 1)
   end
+end
 
-  it "has a label", points: 2 do
+feature "First RCAV for square root form" do
+  it "has a label", points: 1, hint: h("copy_must_match") do
     visit "/square_root/new"
 
     expect(page).to have_css("label", text: "Enter a number")
   end
+end
 
-  it "has an input", points: 2 do
+feature "First RCAV for square root form" do
+  it "has an input", points: 1 do
     visit "/square_root/new"
 
-    expect(page).to have_css("input")
+    expect(page).to have_css("input", count: 1)
   end
+end
 
-  it "has a button with the correct copy", points: 2, hint: I18n.t("hints.button_text") do
+feature "First RCAV for square root form" do
+  it "has a button", points: 1, hint: h("copy_must_match") do
     visit "/square_root/new"
 
     expect(page).to have_css("button", text: "Calculate square root")
   end
+end
 
-  it "when submitted leads to some other URL", points: 4, hint: I18n.t("hints.button_type") do
+feature "First RCAV for square root form" do
+  it "has an action attribute", points: 1 do
     visit "/square_root/new"
 
-    expect(page).to have_css("form[action]")
+    expect(page).to have_css("form[action]", count: 1)
   end
+end
 
-  it "captures the user's input in the query string with a name", points: 4 do
+feature "First RCAV for square root form" do
+  it "when submitted leads to another functional RCAV", points: 6, hint: h("button_type") do
     visit "/square_root/new"
 
-    expect(page).to have_css("input[name]")
-  end
+    click_button "Calculate square root"
 
-  it "works with 5", points: 6, hint: I18n.t("hints.label_for_input") do
+    expect(page).to_not have_current_path("/square_root/new", only_path: true)
+  end
+end
+
+feature "First RCAV for square root form" do
+  it "captures the user's input in the query string", points: 4, hint: h("names_for_inputs") do
+    visit "/square_root/new"
+
+    expect(page).to have_css("input[name]", count: 1)
+  end
+end
+
+feature "Second RCAV for square root form" do
+  it "correctly processes an input of 5", points: 3, hint: h("label_for_input params_are_strings") do
     visit "/square_root/new"
 
     fill_in "Enter a number", with: 5
 
     click_button "Calculate square root"
 
-    expect(page).to have_content("2.236")
-  end
-
-  it "works with 6", points: 8, hint: I18n.t("hints.label_for_input") do
-    visit "/square_root/new"
-
-    fill_in "Enter a number", with: 8
-
-    click_button "Calculate square root"
-
-    expect(page).to have_content("2.828")
+    expect(page).to have_content(2.236)
   end
 end
 
-feature "Payment form" do
+feature "Second RCAV for square root form" do
+  it "correctly processes an input of 42.42", points: 3, hint: h("label_for_input params_are_strings") do
+    visit "/square_root/new"
+
+    fill_in "Enter a number", with: 42.42
+
+    click_button "Calculate square root"
+
+    expect(page).to have_content(6.513)
+  end
+end
+
+feature "First RCAV for payment form" do
   it "has a form element", points: 1 do
     visit "/payment/new"
 
-    expect(page).to have_css("form")
+    expect(page).to have_css("form", count: 1)
   end
+end
 
-  it "has a label for APR", points: 1 do
+feature "First RCAV for payment form" do
+  it "has a label for APR", points: 1, hint: h("copy_must_match") do
     visit "/payment/new"
 
     expect(page).to have_css("label", text: "APR")
   end
+end
 
-  it "has a label for number of years", points: 1 do
+feature "First RCAV for payment form" do
+  it "has a label for number of years", points: 1, hint: h("copy_must_match") do
     visit "/payment/new"
 
     expect(page).to have_css("label", text: "Number of years")
   end
+end
 
-  it "has a label for principal", points: 1 do
+feature "First RCAV for payment form" do
+  it "has a label for principal", points: 1, hint: h("copy_must_match") do
     visit "/payment/new"
 
     expect(page).to have_css("label", text: "Principal")
   end
+end
 
+feature "First RCAV for payment form" do
   it "has three inputs", points: 1 do
     visit "/payment/new"
 
     expect(page).to have_css("input", count: 3)
   end
+end
 
-  it "has a button with the correct copy", points: 1, hint: I18n.t("hints.button_text") do
+feature "First RCAV for payment form" do
+  it "has a button", points: 1, hint: h("copy_must_match") do
     visit "/payment/new"
 
     expect(page).to have_css("button", text: "Calculate monthly payment")
   end
+end
 
-  it "when submitted leads to some other URL", points: 4 do
+feature "First RCAV for payment form" do
+  it "has an action attribute", points: 2 do
     visit "/payment/new"
 
     expect(page).to have_css("form[action]")
   end
+end
 
-  it "captures the user's input in the query string with names", points: 4 do
+feature "First RCAV for payment form" do
+  it "when submitted leads to another functional RCAV", points: 6, hint: h("button_type") do
+    visit "/payment/new"
+
+    click_button "Calculate monthly payment"
+
+    expect(page).to_not have_current_path("/payment/new", only_path: true)
+  end
+end
+
+feature "First RCAV for payment form" do
+  it "captures the user's input in the query string", points: 4, hint: h("names_for_inputs") do
     visit "/payment/new"
 
     expect(page).to have_css("input[name]", count: 3)
   end
+end
 
-  it "works", points: 12, hint: I18n.t("hints.label_for_input") do
+feature "Second RCAV for payment form" do
+  it "correctly processes inputs", points: 3, hint: h("label_for_input params_are_strings") do
     visit "/payment/new"
 
-    fill_in "APR", with: 4.1
+    fill_in "APR", with: 3.24
+    fill_in "Number of years", with: 5
+    fill_in "Principal", with: 14_600
+
+    click_button "Calculate monthly payment"
+
+    expect(page).to have_content(263.90)
+  end
+end
+
+feature "Second RCAV for payment form" do
+  it "rounds the payment", points: 1, hint: h("round") do
+    visit "/payment/new"
+
+    fill_in "APR", with: 3.24
+    fill_in "Number of years", with: 5
+    fill_in "Principal", with: 14_503
+
+    click_button "Calculate monthly payment"
+
+    expect(page).to have_content(262.15)
+  end
+end
+
+feature "Second RCAV for payment form" do
+  it "formats large dollar amounts", points: 2, hint: h("number_to_currency") do
+    visit "/payment/new"
+
+    fill_in "APR", with: 4.10
     fill_in "Number of years", with: 30
-    fill_in "Principal", with: 250000
+    fill_in "Principal", with: 250_000
 
     click_button "Calculate monthly payment"
 
@@ -228,70 +344,96 @@ feature "Payment form" do
   end
 end
 
-feature "Random form" do
+feature "Second RCAV for payment form" do
+  it "rounds the interest rate to four digits", points: 1, hint: h("round") do
+    visit "/payment/new"
+
+    fill_in "APR", with: 4.12345
+    fill_in "Number of years", with: 30
+    fill_in "Principal", with: 250_000
+
+    click_button "Calculate monthly payment"
+
+    expect(page).to have_content(4.1235)
+  end
+end
+
+feature "Second RCAV for payment form" do
+  it "formats the rate as a percentage", points: 1, hint: h("number_to_percentage") do
+    visit "/payment/new"
+
+    fill_in "APR", with: 4.12345
+    fill_in "Number of years", with: 30
+    fill_in "Principal", with: 250_000
+
+    click_button "Calculate monthly payment"
+
+    expect(page).to have_content("4.1235%")
+  end
+end
+
+feature "First RCAV for random form" do
   it "has a form element", points: 1 do
     visit "/random/new"
 
-    expect(page).to have_css("form")
+    expect(page).to have_css("form", count: 1)
   end
+end
 
-  it "has a label for minimum", points: 1 do
+feature "First RCAV for random form" do
+  it "has a label for minimum", points: 1, hint: h("copy_must_match") do
     visit "/random/new"
 
     expect(page).to have_css("label", text: "Minimum")
   end
+end
 
-  it "has a label for maximum", points: 1 do
+feature "First RCAV for random form" do
+  it "has a label for maximum", points: 1, hint: h("copy_must_match") do
     visit "/random/new"
 
     expect(page).to have_css("label", text: "Maximum")
   end
+end
 
+feature "First RCAV for random form" do
   it "has two inputs", points: 1 do
     visit "/random/new"
 
     expect(page).to have_css("input", count: 2)
   end
+end
 
-  it "has a button with the correct copy", points: 1, hint: I18n.t("hints.button_text") do
+feature "First RCAV for random form" do
+  it "has a button", points: 1, hint: h("copy_must_match") do
     visit "/random/new"
 
     expect(page).to have_css("button", text: "Pick random number")
   end
+end
 
-  it "when submitted leads to some other URL", points: 4 do
+feature "First RCAV for random form" do
+  it "has an action attribute", points: 1 do
     visit "/random/new"
 
     expect(page).to have_css("form[action]")
   end
+end
 
-  it "captures the user's input in the query string with names", points: 4 do
+feature "First RCAV for random form" do
+  it "when submitted leads to another functional RCAV", points: 6, hint: h("button_type") do
+    visit "/random/new"
+
+    click_button "Pick random number"
+
+    expect(page).to_not have_current_path("/random/new", only_path: true)
+  end
+end
+
+feature "First RCAV for random form" do
+  it "captures the user's input in the query string", points: 4, hint: h("names_for_inputs") do
     visit "/random/new"
 
     expect(page).to have_css("input[name]", count: 2)
-  end
-
-  it "has a \"Recalculate\" link", points: 1 do
-    visit "/random/new"
-
-    click_button "Pick random number"
-
-    expect(page).to have_css("a", text: "Recalculate")
-  end
-
-  it "has a \"Recalculate\" link that works", points: 0 do
-    visit "/random/new"
-
-    fill_in "Minimum", with: -2.5
-
-    fill_in "Maximum", with: 3.5
-
-    click_button "Pick random number"
-
-    original_results_url = page.current_url
-
-    click_link "Recalculate"
-
-    expect(page).to have_current_path(original_results_url, url: true)
   end
 end
