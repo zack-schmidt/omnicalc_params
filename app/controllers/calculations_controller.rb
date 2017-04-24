8a2f6c0aa4287ac5604e6e5233b72c40c017e1c6
Zack Schmidt <zack.schmidt@kellogg.northwestern.edu>
2017-04-24 12:27:54 -0500

    pt 1 + p2

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,22 +2,31 @@ class CalculationsController < ApplicationController
 
 
 
-  def flex_square
+  def flexible_square
     @the_number = params["num"].to_f
-    render("calculations/flex_square.html.erb")
+    @the_number_squared = @the_number ** 2
+    render("calculations/flexible_square.html.erb")
   end
 
-  def flex_square_root
+  def flexible_square_root
     @the_number = params["num"].to_f
-    render("calculations/flex_square_root.html.erb")
+    @the_number_route = @the_number ** 0.5
+    render("calculations/flexible_square_root.html.erb")
   end
 
-  def flex_payment
+  def flexible_payment
     @the_basis_points = params["basis_points"].to_f
     @the_number_of_years = params["number_of_years"].to_f
     @the_present_value = params["present_value"].to_f
-    @the_monthly_payment = (@the_present_value*@the_basis_points/1200)/((1-((1+(@the_basis_points/1200))**(-@the_number_of_years*12))))
-    render("calculations/flex_payment.html.erb")
+    @the_monthly_payment = (@the_present_value*@the_basis_points/120000)/((1-((1+(@the_basis_points/120000))**(-@the_number_of_years*12))))
+    render("calculations/flexible_payment.html.erb")
+  end
+
+  def flexible_random
+    @the_minimum = params["min"].to_f
+    @the_maximum = params["max"].to_f
+    @the_random_number = rand(@the_minimum...@the_maximum)
+    render("calculations/flexible_random.html.erb")
   end
 
   def square_form
@@ -25,7 +34,42 @@ class CalculationsController < ApplicationController
   end
 
   def square_results
+    @the_number = params["user_number"].to_f
+    @the_number_squared = @the_number ** 2
     render("calculations/square_results.html.erb")
+  end
+
+  def square_root_form
+    render("calculations/square_root_form.html.erb")
+  end
+
+  def square_root_results
+    @the_number = params["user_number"].to_f
+    @the_number_squared = @the_number ** 0.5
+    render("calculations/square_root_results.html.erb")
+  end
+
+  def payment_form
+    render("calculations/payment_form.html.erb")
+  end
+
+  def payment_results
+    @the_basis_points = params["user_apr"].to_f
+    @the_number_of_years = params["user_number_of_years"].to_f
+    @the_present_value = params["user_principal"].to_f
+    @the_monthly_payment = (@the_present_value*@the_basis_points/1200)/((1-((1+(@the_basis_points/1200))**(-@the_number_of_years*12))))
+    render("calculations/payment_results.html.erb")
+  end
+
+  def random_form
+    render("calculations/random_form.html.erb")
+  end
+
+  def random_results
+    @the_minimum = params["user_min"].to_f
+    @the_maximum = params["user_max"].to_f
+    @the_random_number = rand(@the_minimum...@the_maximum)
+    render("calculations/random_results.html.erb")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get("/flexible/square/:num", { :controller => "calculations", :action => "flex_square"})
-  get("/flexible/square_root/:num", { :controller => "calculations", :action => "flex_square_root"})
-  get("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "calculations", :action => "flex_payment"})
+  get("/flexible/square/:num", { :controller => "calculations", :action => "flexible_square"})
+  get("/flexible/square_root/:num", { :controller => "calculations", :action => "flexible_square_root"})
+  get("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "calculations", :action => "flexible_payment"})
+  get("flexible/random/:min/:max", { :controller => "calculations", :action => "flexible_random"})
   get("/square/new", { :controller => "calculations", :action => "square_form"})
   get("/square/results", { :controller => "calculations", :action => "square_results"})
+  get("/square_root/new", { :controller => "calculations", :action => "square_root_form"})
+  get("/square_root/results", { :controller => "calculations", :action => "square_root_results"})
+  get("/payment/new", { :controller => "calculations", :action => "payment_form"})
+  get("/payment/results", { :controller => "calculations", :action => "payment_results"})
+  get("/random/new", { :controller => "calculations", :action => "random_form"})
+  get("/random/results", { :controller => "calculations", :action => "random_results"})
 
 end
